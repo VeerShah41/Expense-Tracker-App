cab921ac5788444495fd1f668a4a1d13b901d05d
Veer Shah <veershah@Veers-MacBook-Pro.local>
2025-09-27 00:21:12 +0530

    Navagitions added

--- mobile-frontend/App.js
+++ mobile-frontend/App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// all required imports 
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import React from 'react';
+// Removed unused imports
+
+
+// all required imports for todo app => ( components )
+import Home from './src/screens/tabs/Home';
+import AddExpences from './src/screens/tabs/AddExpense';
+import Visualize from './src/screens/tabs/Visualize'; 
+import SettingsScreen from './src/screens/SettingScreen';
+import Profile from './src/screens/Profile';
+
+const tab = createBottomTabNavigator();
+const drawer = createDrawerNavigator();
+// app builder function
+function MyTabs() {
+  return (
+    <tab.Navigator>
+      <tab.Screen name="All Expenses" component={Home} />
+      <tab.Screen name="Entry Expenses" component={AddExpences} />
+      <tab.Screen name="Visualize" component={Visualize} />
+    </tab.Navigator>
+  );
+
+}
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      
+      <drawer.Navigator>
+        <drawer.Screen name="Expense Tracker" component={MyTabs} />
+        
+        <drawer.Screen name="Profile" component={Profile} />
+        <drawer.Screen name="Settings" component={SettingsScreen} />
+      </drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ mobile-frontend/src/screens/tabs/AddExpense.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+export default function AddExpences() { 
+  return (
+    <View>
+      <Text>Add Expenses</Text>
+    </View>
+  );
+}
